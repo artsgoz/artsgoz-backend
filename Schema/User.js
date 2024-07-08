d8527ca68841e011e95bab4c7857eb4801d3e076
Dhana <105502355+dhanavadh@users.noreply.github.com>
2024-07-08 16:48:06 +0700

    edit user_image
    edit user_image to staff_image https://www.arts.chula.ac.th/goz/asset/staff_image.png as default img

--- Schema/User.js
+++ Schema/User.js
@@ -30,7 +30,7 @@ const userSchema = mongoose.Schema({
             type: String,
             default: () => {
                 // return `https://api.dicebear.com/6.x/${profile_imgs_collections_list[Math.floor(Math.random() * profile_imgs_collections_list.length)]}/svg?seed=${profile_imgs_name_list[Math.floor(Math.random() * profile_imgs_name_list.length)]}`
-                return `https://www.arts.chula.ac.th/goz/asset/user_image.png`
+                return `https://www.arts.chula.ac.th/goz/asset/staff_image.png`
             } 
         },
     },
